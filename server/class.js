403b63666e9b39e471f9c1b513b18439a1d565f1
Ibrahim Muhammad <bibcodes5@gmail.com>
2024-09-06 08:15:43 +0100

    commited

--- server/class.js
+++ server/class.js
@@ -11,3 +11,14 @@ class User {
     }
 }
 module.exports=User;
+
+let person = [
+    new User("ibrahim", 56 ),
+    new User("muhammad", 47),
+    new User("Zainab", 8 ,"Female"),
+    new User("Khadija", 10, "Female" )
+];
+// console.log(person);
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const User = require('./class');
+const port = 4000;
 
 const app =express();
+
 
 app.get('/',(req,res)=>{
     console.log('Thank you for using our server');
@@ -9,12 +11,31 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/users',(req,res)=>{
-    console.log('Getting users'); 
+    console.log('Getting users');  
     const users =[new User('Ibrahim', 10),new User('Aisha',5,'Female')];
     res.send(users);
 })
 
-app.listen(3000, ()=>{
-    console.log('Congratulation you have created a server');
+
+app.get('/download',(req,res)=>{
+    console.log('Loading download content');
+    res.download('Js-for-kids.pdf',console.error())
+    res.send('Downloading your content')
+    
 })
 
+
+app.delete("/delete", (req, res) => {
+    console.log("DELETE REQUEST SUCCESSFUL");
+    const user= User();
+    // console.log(req.body);
+    // res._destroy(user);
+})
+
+
+app.listen(port, ()=>{
+    console.log('Congratulation you have created a server');
+})
+//8C52FF
+
+//The HTTP OPTIONS method requests permitted communication options for a given URL or server.
